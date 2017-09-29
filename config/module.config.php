03368823ec9f8765b7ea557b8d69a9246488d0cb
Judzhin Miles <info@msbios.com>
2017-09-29 14:13:38 +0300

    Lazy Composer Commiter

--- config/module.config.php
+++ config/module.config.php
@@ -17,11 +17,16 @@ return [
 
     'form_elements' => [
         'factories' => [
-            Form\LayoutForm::class => InvokableFactory::class,
-            Form\ModuleForm::class => InvokableFactory::class,
-            Form\PageTypeForm::class => InvokableFactory::class,
-            Form\RouteForm::class => InvokableFactory::class,
-            Form\ThemeForm::class => InvokableFactory::class,
+            Form\LayoutForm::class =>
+                InvokableFactory::class,
+            Form\ModuleForm::class =>
+                InvokableFactory::class,
+            Form\PageTypeForm::class =>
+                InvokableFactory::class,
+            Form\RouteForm::class =>
+                InvokableFactory::class,
+            Form\ThemeForm::class =>
+                InvokableFactory::class,
         ],
         'initializers' => [
             new Initializer\LazyFormInitializer
@@ -30,13 +35,18 @@ return [
 
     'input_filters' => [
         'factories' => [
-            InputFilter\ModuleInputFilter::class => InvokableFactory::class,
-            InputFilter\TemplateInputFilter::class => InvokableFactory::class,
+            InputFilter\ModuleInputFilter::class =>
+                InvokableFactory::class,
+            InputFilter\TemplateInputFilter::class =>
+                InvokableFactory::class,
         ],
         'aliases' => [
-            Form\LayoutForm::class => InputFilter\TemplateInputFilter::class,
-            Form\ModuleForm::class => InputFilter\ModuleInputFilter::class,
-            Form\ThemeForm::class => InputFilter\TemplateInputFilter::class,
+            Form\LayoutForm::class =>
+                InputFilter\TemplateInputFilter::class,
+            Form\ModuleForm::class =>
+                InputFilter\ModuleInputFilter::class,
+            Form\ThemeForm::class =>
+                InputFilter\TemplateInputFilter::class,
         ]
     ],
 
