684cd1131a3a8bcb93d8e49de9f5497887f4d36a
Judzhin Miles <info@msbios.com>
2018-08-15 15:35:03 +0300

    Lazy Composer Commiter

--- src/RecordRepository.php
+++ src/RecordRepository.php
@@ -5,10 +5,14 @@
  */
 namespace MSBios\Resource;
 
+use MSBios\Db\TableGateway\TableGateway;
 use MSBios\Db\TableGateway\TableGatewayInterface;
 use MSBios\Resource\Exception\RowNotFoundException;
 use Zend\Db\ResultSet\ResultSet;
+use Zend\Db\Sql\Select;
+use Zend\Db\Sql\Sql;
 use Zend\Paginator\Adapter\AdapterInterface;
+use Zend\Paginator\Adapter\DbSelect;
 use Zend\Paginator\Adapter\DbTableGateway as TableGatewayPaginator;
 use Zend\Paginator\Paginator;
 use Zend\Stdlib\ArrayObject;
@@ -55,6 +59,45 @@ class RecordRepository implements RecordRepositoryInterface
 
         return $row;
     }
+
+    // /**
+    //  * @param null $where
+    //  * @param null $order
+    //  * @param null $group
+    //  * @param null $having
+    //  * @return Paginator
+    //  */
+    // public function fetchAll($where = null, $order = null, $group = null, $having = null)
+    // {
+    //     /** @var TableGatewayInterface|TableGateway $tableGateway */
+    //     $tableGateway = $this->tableGateway;
+    //
+    //     /** @var Sql $sql */
+    //     $sql = $tableGateway->getSql();
+    //
+    //     /** @var Select $select */
+    //     $select = $sql->select();
+    //
+    //     if ($where) {
+    //         $select->where($where);
+    //     }
+    //
+    //     if ($order) {
+    //         $select->order($order);
+    //     }
+    //
+    //     if ($group) {
+    //         $select->group($group);
+    //     }
+    //
+    //     if ($having) {
+    //         $select->having($having);
+    //     }
+    //
+    //     /** @var AdapterInterface $adapter */
+    //     $adapter = new DbSelect($select, $sql);
+    //     return new Paginator($adapter);
+    // }
 
     /**
      * @param null $where
